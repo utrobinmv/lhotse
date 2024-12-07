520d23ca5b7f9918baf6f81009a63e6cd19383df
joefox <utrobinmv@yandex.ru>
2024-12-08 02:06:26 +0300

    add cut utils

--- lhotse/src/utils/__init__.py
+++ lhotse/src/utils/__init__.py
@@ -1,3 +1,3 @@
 from .file_utils import (get_file_extension, make_dirs, dict_to_json)
-from .cut_utils import (get_text_from_batch, get_it_values)
+from .cut_utils import (get_text_from_batch, get_it_values, get_align_from_batch, create_futures_labels)
 from .iter_dataset import (IterableDatasetWithLang, IterableDatasetRandom)
--- lhotse/src/utils/cut_utils.py
+++ lhotse/src/utils/cut_utils.py
@@ -1,9 +1,17 @@
 from typing import LiteralString
+import numpy as np
 import lhotse
 
 
+def _get_feat_extract_output_lengths(input_lengths: int):
+    """
+    Computes the output length of the convolutional layers for Whisper
+    """
+    input_lengths = (input_lengths - 1) // 2 + 1
 
-def get_it_values(it, custom_keys=[]) -> list[dict[str, str|float]]:
+    return input_lengths
+
+def get_it_values(it, custom_keys=[], align_keys=[]) -> list[dict[str, str|float]]:
     """
     Преобразует список кутов в список значений, для последующего извлечения информации
     """
@@ -11,7 +19,7 @@ def get_it_values(it, custom_keys=[]) -> list[dict[str, str|float]]:
         #print('mix',it.cut)
         add_it = {'offset': it.offset,
                   'type': 'offset'}        
-        return [add_it] + get_it_values(it.cut, custom_keys)
+        return [add_it] + get_it_values(it.cut, custom_keys, align_keys)
     elif isinstance(it, lhotse.cut.mono.MonoCut):
         if len(it.supervisions) > 0:
             list_timeline = []
@@ -34,6 +42,11 @@ def get_it_values(it, custom_keys=[]) -> list[dict[str, str|float]]:
                         add_it[custom_key] = supervision.custom[custom_key]
                     else:
                         add_it[custom_key] = ''
+                for custom_key in align_keys:
+                    if custom_key in supervision.alignment.keys():
+                        add_it[custom_key] = supervision.alignment[custom_key]
+                    else:
+                        add_it[custom_key] = ''
                 list_timeline.append(add_it)
             return list_timeline
         if it.recording:
@@ -48,7 +61,7 @@ def get_it_values(it, custom_keys=[]) -> list[dict[str, str|float]]:
         #print('MixedCut:',it)
         list_timeline = []
         for mix_track in it.tracks:
-            list_add_it = get_it_values(mix_track, custom_keys)
+            list_add_it = get_it_values(mix_track, custom_keys, align_keys)
             
             list_timeline.extend(list_add_it)
             #print(calc_duration(list_add_it))
@@ -96,3 +109,65 @@ def get_text_from_batch(batch_cuts, pause_token=' ', text_column:str = None, wit
                 list_text.append(f'<|{end_text:.2f}|>')
     text = ''.join(list_text)    
     return text
+
+def get_align_from_batch(batch_cuts, align_column:str = None) -> str:
+    """
+    Вытягивает из списка информации о aligned временных метках
+    """
+    list_timeline = get_it_values(batch_cuts, align_keys=[align_column])
+    list_text = []
+    end_text = 0
+    offset = 0
+    for time_item in list_timeline:
+        if 'type' in time_item.keys() and time_item['type'] == 'offset':
+            offset = time_item['offset']
+        if 'type' in time_item.keys() and time_item['type'] == 'pause':
+            pass
+        if 'type' in time_item.keys() and time_item['type'] == 'supervision':
+            if time_item[align_column]:
+                for align_item in time_item[align_column]:
+                    dict_item = {'start': align_item.start,
+                                 'duration': align_item.duration,
+                                 'symbol': align_item.symbol,
+                                 'speaker': time_item['speaker'],
+                                 'score': align_item.score}
+                    dict_item['start'] += offset
+                    list_text.append(dict_item)
+            else:
+                pass
+            end_text = offset + time_item['duration']
+    return list_text, end_text
+
+def create_futures_labels(batch_cuts, align_column, hop_length, sample_rate, class_column = None, max_class = 1):
+    '''
+    Данный класс специально заточен под Whisper
+    из за функции _get_feat_extract_output_lengths
+    '''
+    time_line, duration = get_align_from_batch(batch_cuts, align_column)
+    time_calc = lambda x: int(_get_feat_extract_output_lengths(x*sample_rate//hop_length))
+    time_duration = time_calc(duration)
+    if class_column is None: # VAD
+        np_labels = np.zeros(time_duration, dtype=np.int32)
+    else:
+        np_labels = np.zeros([time_duration, max_class], dtype=np.int32)
+    list_class = []
+    for time_item in time_line:
+        time_start = time_item['start']
+        time_end = time_item['start'] + time_item['duration']
+        time_start = time_calc(time_start)
+        time_end = time_calc(time_end)
+        
+        time_start = max(0, time_start)
+        time_end = min(time_duration, time_end)
+        
+        if class_column:
+            class_item = time_item[class_column]
+            if not class_item in list_class:
+                list_class.append(class_item)
+            index_class = list_class.index(class_item)
+            if index_class < max_class:
+                np_labels[time_start:time_end,index_class] = 1
+        else:    
+            np_labels[time_start:time_end] = 1
+        
+    return np_labels[np.newaxis,...]
